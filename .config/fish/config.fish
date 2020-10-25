dbbf2724e044dc4c9d5dbba952fa62265911691c
miyabihito <miyabihito.dev@gmail.com>
2020-10-25 18:07:40 +0900

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+# prompt settings
+set -g theme_date_format "+%Y/%m/%d %T"
+set -g theme_date_timezone Asia/Tokyo
+set -g theme_display_git_master_branch yes
+set -g theme_display_k8s_context yes
+set -g theme_color_scheme dracula
+
+# enviornment variables
+set -x LSCOLORS gxfxcxdxbxegedabagacad
+
+alias vim nvim
